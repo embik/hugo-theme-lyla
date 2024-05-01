fbc9b7013db1ad385238b7ac0a09b4cd3e43edf6
Marvin Beckers <mail@embik.me>
2024-05-01 12:37:54 +0200

    Remove user-mode for color scheme, set in js
    Signed-off-by: Marvin Beckers <mail@embik.me>

--- assets/js/colormode.js
+++ assets/js/colormode.js
@@ -2,27 +2,20 @@
 
 function toggleColorMode(e) {
     // Switch to Light Mode
-    if (e.currentTarget.classList.contains("light--hidden") && e.currentTarget.classList.contains("dark--hidden")) {
+    if (e.currentTarget.classList.contains("light--hidden")) {
     	// Sets the custom HTML attribute
     	document.documentElement.setAttribute("color-mode", "light");
 
 		//Sets the user's preference in local storage
 		localStorage.setItem("color-mode", "light")
 		return;
-	} else if (e.currentTarget.classList.contains("dark--hidden") && e.currentTarget.classList.contains("user--hidden")) {
+	} else {
         /* Switch to Dark Mode
         Sets the custom HTML attribute */
         document.documentElement.setAttribute("color-mode", "dark");
 
         // Sets the user's preference in local storage
         localStorage.setItem("color-mode", "dark");
-    } else {
-        /* Switch to User Mode
-        Sets the custom HTML attribute */
-        document.documentElement.setAttribute("color-mode", "user");
-
-        // Sets the user's preference in local storage
-        localStorage.setItem("color-mode", "user");
     }
 };
 
@@ -35,5 +28,13 @@ toggleColorButtons.forEach(btn => {
 });
 
 if ("color-mode" in localStorage) {
+    document.documentElement.setAttribute('color-mode', localStorage.getItem('color-mode'));
+} else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    // Sets the user's preference in local storage
+    localStorage.setItem("color-mode", "dark");
+    document.documentElement.setAttribute('color-mode', localStorage.getItem('color-mode'));
+} else {
+    // Sets the user's preference in local storage
+    localStorage.setItem("color-mode", "light");
     document.documentElement.setAttribute('color-mode', localStorage.getItem('color-mode'));
 }
